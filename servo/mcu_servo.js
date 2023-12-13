1af4e87ccf4e259f6b7fe89eba97b38cc05bd0a8
404background <404background@gmail.com>
2023-12-13 17:08:10 +0900

    ノードの形状を変更

--- servo/mcu_servo.js
+++ servo/mcu_servo.js
@@ -1,7 +1,19 @@
 module.exports = function(RED) {
     function ServoNode(config) {
+        var node = this;
         RED.nodes.createNode(this, config);
-		console.log(config)
+        let Hz = config.hz;
+        let PulseMin = config.pulseMin;
+        let PulseMax = config.pulseMax;
+        let AngleMin = config.angleMin;
+        let AngleMax = config.angleMax;
+        let cycle = 1 / Hz;
+        let PulseWidth = PulseMax-PulseMin;
+        let AngleWidth = AngleMax-AngleMin;
+        node.on('input', function(msg) {
+            msg.payload = msg.payload / AngleWidth * PulseWidth / cycle;
+            node.send(msg);
+        });
     }
     RED.nodes.registerType("mcu_servo", ServoNode);
 }
