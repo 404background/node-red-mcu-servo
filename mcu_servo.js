2c251a622ce135afb1649c912535e8ffce23ce98
404background <404background@gmail.com>
2023-12-13 18:25:06 +0900

    数値型に変更

--- mcu_servo.js
+++ mcu_servo.js
@@ -2,11 +2,11 @@ module.exports = function(RED) {
     function ServoNode(config) {
         var node = this;
         RED.nodes.createNode(this, config);
-        let Hz = config.hz;
-        let PulseMin = config.pulseMin;
-        let PulseMax = config.pulseMax;
-        let AngleMin = config.angleMin;
-        let AngleMax = config.angleMax;
+        let Hz = Number(config.hz);
+        let PulseMin = Number(config.pulseMin);
+        let PulseMax = Number(config.pulseMax);
+        let AngleMin = Number(config.angleMin);
+        let AngleMax = Number(config.angleMax);
         let cycle = 1 / Hz;
         let PulseWidth = PulseMax-PulseMin;
         let AngleWidth = AngleMax-AngleMin;
